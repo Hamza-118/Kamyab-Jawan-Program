aeabea75d1d5a3961fcc4f8462d5361d82d84307
Hamza Ahmed Khan <60402389+Hamza-118@users.noreply.github.com>
2020-12-03 18:35:08 +0500

    led ON on 1 and OFF on 0.
    Arduino code on controller.

---
+++ high_low_on_real_arduino.ino.ino
@@ -0,0 +1,19 @@
+char a;
+void setup() {
+  // put your setup code here, to run once:
+pinMode (13, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  char a= Serial.read();
+ if (Serial.available ())
+  // put your main code here, to run repeatedly:
+a = digitalRead (13);
+if (a=='1'){
+  digitalWrite (13,HIGH);
+}
+else if (a=='0'){
+  digitalWrite(13,LOW);
+}
+}
